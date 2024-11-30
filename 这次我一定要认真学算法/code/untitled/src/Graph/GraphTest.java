03a2afbf1cfd4d1e89cd368bb5fc9cbba2e0fbaf
hanxing_go <1209978808@qq.com>
2024-11-30 20:40:52 +0800

    图算法的再学

--- 这次我一定要认真学算法/code/untitled/src/Graph/GraphTest.java
+++ 这次我一定要认真学算法/code/untitled/src/Graph/GraphTest.java
@@ -12,7 +12,8 @@ public class GraphTest {
         if (node == null) {
             return;
         }
-        HashSet<Node> set = new HashSet<>();//用来存储是否存在过某个节点
+        HashSet<Node> set = new HashSet<>();
+        //用来存储是否存在过某个节点
         Queue<Node> queue = new LinkedList<>();
         queue.add(node);
         set.add(node);
@@ -45,8 +46,8 @@ public class GraphTest {
                 if (!set.contains(next)) {
                     System.out.println(next.value);
                     set.add(next);
-                    stack.push(cur);
-                    stack.push(cur);
+                    stack.push(cur);//因为当前节点可能有好多个节点，重新把当前节点压入栈中
+                    stack.push(next);//在把当前节点的下一个节点压入栈中
                     break;
                 }
             }
@@ -54,8 +55,96 @@ public class GraphTest {
     }
 
     //3. 拓扑排序算法
-    public static void topologtSort() {
+    public static List<Node> topologtSort(Graph graph) {
+        //核心思想是先找到入度为0的点
 
+        //先创建一个要返回的List集合
+        List<Node> nodelist = new ArrayList<>();
+        HashMap<Node, Integer> inMap = new HashMap<>();
+        Queue<Node> zeroInQueue = new LinkedList<>();
+        //先找到入度为0的点
+        for (Node node : graph.nodes.values()) {
+            inMap.put(node, node.in);
+            if (node.in == 0) {
+                zeroInQueue.add(node);
+                //如果入度为0就把他加到这个队列
+            }
+        }
+
+
+        while (!zeroInQueue.isEmpty()) {
+            Node cur = zeroInQueue.poll();
+            //弹出当前入度为0的点，并且擦除他的影响
+            nodelist.add(cur);
+            for (Node next : cur.nexts) {
+                inMap.put(next, inMap.get(next) - 1);
+                //如果入度为0则加入队列
+                if (inMap.get(next) == 0) {
+                    zeroInQueue.add(next);
+                }
+            }
+        }
+        return nodelist;
+    }
+
+    // 4. K算法解决最小生成树问题
+    public static class MySets{
+        public HashMap<Node, List<Node>> setMap;
+        public MySets(List<Node> nodes) {
+            for (Node cur : nodes) {
+                List<Node> set = new ArrayList<Node>();
+                set.add(cur);//每一个节点，对应一个单独的集合
+                setMap.put(cur,set);
+            }
+        }
+
+        public boolean isSameSet(Node from, Node to) {
+            List<Node> fromSet = setMap.get(from);
+            List<Node> toSet = setMap.get(to);
+
+            return fromSet == toSet;
+        }
+
+        public void union(Node from, Node to) {
+            List<Node> fromSet = setMap.get(from);
+            List<Node> toSet = setMap.get(to);
+
+            for (Node toNode : toSet) {
+                fromSet.add(toNode);//将每一个to节点都加入到from集合中
+                setMap.put(toNode, fromSet);//同时把toNode对应的集合全部更新为fromSet
+            }
+        }
+    }
+
+    public static Set<Edge> kruskalMST(Graph graph) {
+        List<Node> nodes = new ArrayList<>();
+        Collection<Node> values = graph.nodes.values();
+        Iterator<Node> it = values.iterator();
+        while (it.hasNext()) {
+            nodes.add(it.next());
+        }
+        MySets mySets = new MySets(nodes);
+        //利用一个优先队列，其实就是一个堆结构
+        PriorityQueue<Edge> priorityQueue = new PriorityQueue<>(new Comparator<Edge>() {
+            @Override
+            public int compare(Edge o1, Edge o2) {
+                return o1.weight - o2.weight;
+            }
+        });
+
+        for (Edge edge : graph.edges) {
+            priorityQueue.add(edge);//将每一条边加入到优先队列中
+        }
+        Set<Edge> res = new HashSet<>();
+        while (!priorityQueue.isEmpty()) {
+            Edge e = priorityQueue.poll();
+            //找到最小边的from点和to点，判断是否在一个集合
+            if (!mySets.isSameSet(e.from, e.to)) {
+                mySets.union(e.from, e.to);
+                res.add(e);
+            }
+        }
+        return res;
     }
 }
 
--- 这次我一定要认真学算法/code/untitled/src/Main.java
+++ 这次我一定要认真学算法/code/untitled/src/Main.java
@@ -1,12 +1,42 @@
+import java.io.*;
+import java.lang.reflect.GenericDeclaration;
 import java.util.*;
-import java.util.function.Function;
-import java.util.function.IntFunction;
-import java.util.stream.Collectors;
+import java.util.function.BiConsumer;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        // 把文章顺序进行恢复到一个新文件中
 
+        //1.第一步一定是先读入文件
+        BufferedReader fis = new BufferedReader(new FileReader("Src\\a.txt"));
+        BufferedWriter fos = new BufferedWriter(new FileWriter("Src\\b.txt"));
+
+        // 2. 根据需求，我们可以发现他是一行一句话，那么我们可以用读整行的方法
+        Map<Integer, String> s = new TreeMap<>();
+        String readstring;
+        while ((readstring = fis.readLine()) != null) {
+            System.out.println(readstring);
+            if (readstring.length() > 0) {
+                int index = Integer.parseInt(readstring.substring(0,1));
+                s.put(index, readstring);
+            }
+        }
+        byte[] bytes = new byte[1024];
+
+        s.forEach(new BiConsumer<Integer, String>() {
+            @Override
+            public void accept(Integer integer, String s) {
+                try {
+                    fos.write(s);
+                    fos.newLine();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+
+        fis.close();
+        fos.close();
     }
-
 }
 
